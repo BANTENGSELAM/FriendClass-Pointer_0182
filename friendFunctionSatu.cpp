938892bd6f76394159dabe6fe892975eeead46b8
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-27 16:11:00 +0700

    class siswa

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -12,3 +12,13 @@ public:
     void setNama(string Pnama);
     friend class siswa;
 };
+
+class siswa 
+{
+private:
+    int id;
+
+public:
+    void setId(int Pid);
+    void displayAll(orang &a);
+};
