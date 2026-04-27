8050fcfdb0e2590f6aaba8396be2eef7423b68ef
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-27 16:15:10 +0700

    class

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string Pnama);
+    friend class siswa;
+};
+
+class siswa 
+{
+private:
+    int id;
+
+public:
+    void setId(int Pid);
+    void displayAll(orang &a);
+};
