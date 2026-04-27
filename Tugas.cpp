b7f18d6f0a4c09c6136f7106a668182b093eea19
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-04-27 19:42:40 +0700

    Class Layangan

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class BelahKetupat;
+class LayangLayang
+{
+private:
+    int d1, d2; // diagonal
+    int s1, s2, s3, s4; // sisi
+
+public:
+    void input();
+    void luas();
+    void keliling();
+
+    // friend function dari class BelahKetupat
+    friend void BelahKetupat::showKelilingLayang(LayangLayang &l);
+};
